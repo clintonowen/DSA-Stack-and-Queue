a75baaf949d1ec4a2fa0b919524ff6aa38391718
Clinton Owen <clint@clintonowen.com>
2018-10-08 22:26:53 -0500

    Complete drill to remove `Spock` from the queue

--- queue-drills.js
+++ queue-drills.js
@@ -8,7 +8,25 @@ const { Queue, peek, display } = require('./queue');
   for (let i in starTrekData) {
     starTrekQ.enqueue(starTrekData[i]);
   }
-  console.log(starTrekQ);
+  // console.log(starTrekQ);
+
   // console.log(peek(starTrekQ));
+
+  display(starTrekQ);
+
+  // remove 'Spock' from the queue
+  const holdQ = new Queue();
+  let nodeVal = starTrekQ.dequeue();
+  while (nodeVal) {
+    if (nodeVal !== 'Spock') {
+      holdQ.enqueue(nodeVal);
+    }
+    nodeVal = starTrekQ.dequeue();
+  }
+  nodeVal = holdQ.dequeue();
+  while (nodeVal) {
+    starTrekQ.enqueue(nodeVal);
+    nodeVal = holdQ.dequeue();
+  }
   display(starTrekQ);
 })();
